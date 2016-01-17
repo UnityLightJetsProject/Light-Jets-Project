11b79fe1bcdf473f62f4304cfb73b05caeb7abd8
AayLith <bzh-artikodin@hotmail.fr>
2016-01-17 17:07:07 +0100

    fourth

--- Tron Light Jets/Assets/Scripts/Mover.cs
+++ Tron Light Jets/Assets/Scripts/Mover.cs
@@ -4,9 +4,28 @@ using System.Collections;
 public class Mover : MonoBehaviour {
 
 	public float speed;
+    public float destroyDelay;
+    public GameObject explosion;
+    public int damages;
 
 	// Use this for initialization
 	void Start () {
 		GetComponent<Rigidbody>().velocity = transform.forward * speed;
 	}
+
+    void FixedUpdate()
+    {
+        Object.Destroy(gameObject, destroyDelay);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Destructible")
+        {
+           other.transform.parent.SendMessage("damage",damages);
+           Destroy(gameObject);
+        }
+        // Instantiate(explosion, transform.position, transform.rotation);
+        // Destroy(gameObject);
+    }
 }
