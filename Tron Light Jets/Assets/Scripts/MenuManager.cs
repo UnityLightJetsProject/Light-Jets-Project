2bc37f4591e4e43a2add3a09d3fd8cf9a1971dc1
Zakuro Uzueda <zakuro.uzueda@hotmail.fr>
2016-01-18 18:13:21 +0100

    Menu

---
+++ Tron Light Jets/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+	public Canvas exitMenu;
+	public Button startText;
+	public Button exitText;
+
+	void Start ()
+	{
+		exitMenu = exitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		exitMenu.enabled = false;
+	}
+
+	public void ExitConfirmationMenu ()
+	{
+		exitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress ()
+	{
+		exitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartGame ()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit ();
+	}
+}
